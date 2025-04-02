a0b4768fc321bded6bf763686ab51ad5f94277f9
Anjalireddy888 <saumiinturi@gmail.com>
2025-04-02 23:04:25 +0530

    completed multiheadattention

--- model.py
+++ model.py
@@ -70,6 +70,88 @@ class LayerNormalization(nn.Module):
         # add eps to prevent division by zero
         # alpha learns how much to scale (like batch norm's gamma)
         # bias learns how much to shift (like batch norm's beta)
-        return self.alpha *(x - mean) / (std + self.eps) + self.bias
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+    
+    
+class FeedForwardBlock(nn.Module):
+    '''
+    first layer (linear_1) -> expands d_model (small dimension) to d_ff (larger dimension), this gives model more capacity to learn complex features
+    ReLu Activation (torch.relu) -> introduces non-linearity to learn complex features
+    self.dropout -> prevents over-fitting by randomly setting some values to zero
+    linear_2 -> shrinks back from d_ff -> d_model ; to keep dimensions consistent
+    '''
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) # expands dimension    
+        self.dropout = nn.Dropout(dropout) # Applies dropout
+        self.linear_2 = nn.Linear(d_ff, d_model) # shrinks dimension back
+    
+    def forward(self, x):
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+        
+class MultiHeadAttentionBlock(nn.Module):
+    
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model is not divisible by h"
+        
+        self.d_k = d_model // h
+        # linear layers for query, key and value transformations
+        self.w_q = nn.Linear(d_model, d_model) 
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        
+        self.w_o = nn.Linear(d_model, d_model) # output projections
+        self.dropout = nn.Dropout(dropout)
+        
+    @staticmethod
+    def attention (query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        
+        # scaled dot-product attention
+        # (Batch, h, seq_Len, d_k) --> (Batch, h, seq_Len, seq_Len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        
+        return (attention_scores @ value), attention_scores
+        
+        
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (Batch, seq_Len, d_model) --> (Batch, seq_Len, d_model)
+        key = self.w_k(k) # (Batch, seq_Len, d_model) --> (Batch, seq_Len, d_model)
+        value = self.w_v(v) # (Batch, seq_Len, d_model) --> (Batch, seq_Len, d_model)
+        
+        # (Batch, seq_Len, d_model) --> (Batch, seq_Len, h, d_k) --> (Batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1,2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1,2)
+        
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+        
+        # (Batch, h, seq_Len, d_k) --> (batch, seq_Len, h, d_k) --> (Batch, seq_Len, d_model)
+        x = x.transpose(1,2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+        
+        # (Batch, seq_Len, d_model) --> (Batch, seq_Len, d_model)
+        return self.w_o(x)
+    
+class ResidualConnection(nn.Module):
+    
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+        
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+    
+    
+        
+        
     
    
